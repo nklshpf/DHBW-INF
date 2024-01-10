d80db5c142217ecffc389a8f10fb95b602947df6
nklshpf <152854354+nklshpf@users.noreply.github.com>
2024-01-10 17:59:16 +0000

    loop.py change

--- daily-coding/loop.py
+++ daily-coding/loop.py
@@ -1,7 +1,7 @@
-num_rows = int(input("Enter the number of rows: "))
+num_rows = int(input("Anzahl der Reihen eingeben: "))
 
 for i in range(1, num_rows + 1):
     for j in range(1, i + 1):
         print(i, end="")
-    print()  # Move to the next line after printing each row 
+    print()  
 
